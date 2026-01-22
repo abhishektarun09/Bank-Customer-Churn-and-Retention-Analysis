4e55e29e8e06b48d2f0dbd23e56d70935b5e8993
Abhishek Tarun <abhishek.tarun09@gmail.com>
2026-01-22 15:42:23 +0100

    replaced postgres database with google bigquery

--- ingestion_to_db.py
+++ ingestion_to_db.py
@@ -2,14 +2,26 @@ import pandas as pd
 import os
 import time
 import sys
+import pandas_gbq
 
 from src.logger import logging
 from src.exception import CustomException
 from src.postgres_session import get_postgres_connection
+from google.oauth2 import service_account
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gbq_project_id = os.getenv("BigQuery_Project_ID")
+gbq_json_path = os.getenv("BigQuery_Service_Account_JSON")
+gbq_dataset = os.getenv("BigQuery_Dataset")
+
+credentials = service_account.Credentials.from_service_account_file(gbq_json_path)
 
 engine = get_postgres_connection()
 
-def ingest_db(df, table_name, engine):
+def ingest_db_postgres(df, table_name, engine):
     """This function ingests data into SQL Database"""
     
     try:
@@ -17,6 +29,16 @@ def ingest_db(df, table_name, engine):
         
     except Exception as e:
         raise CustomException(e, sys)
+    
+def ingest_db_gbq(df, table_name, project_id, credentials):
+    """This function ingests data into Google BigQuery"""
+    
+    try:
+        pandas_gbq.to_gbq(dataframe=df, destination_table=table_name, project_id=project_id, if_exists='replace', credentials=credentials)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 
 def load_raw_data():    
     """This function will load CSV files as pandas dataframe and ingest into db"""
@@ -27,7 +49,8 @@ def load_raw_data():
             if '.csv' in file:
                 df = pd.read_csv('data/'+file)
                 logging.info(f"Ingesting {file} in db.")
-                ingest_db(df, file[:-4], engine)
+                #ingest_db_postgres(df, file[:-4], engine)
+                ingest_db_gbq(df=df, table_name=f"{gbq_dataset}.{file[:-4]}", project_id=gbq_project_id, credentials=credentials)
         end_time = time.time()
         logging.info("----------Ingestion Completed----------")
         total_time = (end_time - start_time)/60
